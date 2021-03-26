4ad3cd6340c87ba2519c02080a77259ab739067a
prkambar <prkambar@TejasIT-PC4.TISPL.COM>
2021-03-26 14:45:34 +0530

    Committing Test2 to branchX

---
+++ src/Demo/Test2.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Hungry ="yes";
+		if (Hungry =="yes") {
+			System.out.println("eat something...."); 
+		}else {
+			System.out.println("Do work.....");
+		}
+	}
+}
+
+
+
