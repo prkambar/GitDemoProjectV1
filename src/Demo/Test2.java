af4b9da4cf0b543b5a6d5c57e161f6698823b58a
prkambar <prkambar@TejasIT-PC4.TISPL.COM>
2021-03-26 14:56:04 +0530

    Change is added. Two more conditions are added.

--- src/Demo/Test2.java
+++ src/Demo/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 		// TODO Auto-generated method stub
 		
 		String Hungry ="yes";
-		if (Hungry =="yes") {
+		if (Hungry =="yes" || Hungry =="y" || Hungry =="Y") {
 			System.out.println("eat something...."); 
 		}else {
 			System.out.println("Do work.....");
