3d2beb38c6c3fbf8ea1b016e4fb3427335c45c35
prkambar <prkambar@Tejasit-PC8.TISPL.COM>
2021-03-26 13:52:04 +0530

    First Commit....

---
+++ src/Demo/Test.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Hungry ="yes";
+		if (Hungry =="yes") {
+			System.out.println("eat something...."); 
+		}else {
+			System.out.println("Do work.....");
+		}
+	}
+
+}
